61c0cd3e031f6cb00ab46deb9713827da2b20ba6
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-05-28 23:21:51 +0900

    Use Array#delete to improve performance

--- lib/namero/solver.rb
+++ lib/namero/solver.rb
@@ -38,19 +38,19 @@ module Namero
     def fill_candidate_for(v)
       board[v.index, :row].each do |v2|
         unless v2.value
-          v2.candidates -= [v.value]
+          v2.candidates.delete(v.value)
           updated_candidate_queue << v2.index
         end
       end
       board[v.index, :column].each do |v2|
         unless v2.value
-          v2.candidates -= [v.value]
+          v2.candidates.delete(v.value)
           updated_candidate_queue << v2.index
         end
       end
       board[v.index, :block].each do |v2|
         unless v2.value
-          v2.candidates -= [v.value]
+          v2.candidates.delete(v.value)
           updated_candidate_queue << v2.index
         end
       end
